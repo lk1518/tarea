b7032855f414ae6a8acbbf7a9ef127032e3538cd
Leonardo Cabrera <leonardocabrera1518@gmail.com>
2022-10-18 21:27:55 -0400

    Punto 1 de la tarea.

---
+++ Carro.java.java
@@ -0,0 +1,13 @@
+   public class Carro.java{
+    public static void main(String args[]){
+
+        int jesus= 5;
+        int larata= 8;
+        int leo= 13;
+        int sumadelasedades= 0;
+
+        sumadelasedades= jesus + larata + leo;
+
+        System.out.println("la suma de las edades es" + sumadelasedades);
+    }
+        }
